2ae23d609f32e9c14b8f85cab7ecec7421059003
tcast <tony@qcfund.com>
2025-12-20 11:35:40 +0100

    FIX: Update page.tsx import to use @ alias

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useRouter } from 'next/navigation';
-import { trpc } from '../../lib/trpc';
+import { trpc } from '@/lib/trpc';
 import Link from 'next/link';
 import {
   BuildingOfficeIcon,
